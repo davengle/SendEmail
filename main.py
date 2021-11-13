02609f45934cb854d16901b7c4a9524993ef2c35
Dave Engle <davengle@gmail.com>
2021-11-13 21:02:02 +0000

    Send email every 60 seconds

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-Test commit
+import yagmail
+import os
+import time
+
+sender = '1q2w3e4r6900@gmail.com'
+receiver = 'nourwkbrc@emlhub.com'
+
+subject = """
+  This is the subject!
+"""
+
+contents = """
+  HI! Again.  This is a multiline 
+  content message
+"""
+
+while True:
+  yag = yagmail.SMTP(user=sender, password = os.environ['PWGM'])
+  yag.send(to=receiver, subject=subject, contents=contents)
+  print("Email Sent!")
+  time.sleep(60)
